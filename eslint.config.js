7c42c92d7545b6db54dd5200a8b6a1d885f00e0b
Jeremy Mouton <moutonjeremy@labbs.fr>
2026-03-14 22:22:17 +0100

    Add rule to enforce no unused variables in TypeScript files

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,11 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      }],
     },
   },
 )
